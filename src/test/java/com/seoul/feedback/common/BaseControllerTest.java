815ed16c1e39d7cdfa4c82ff536b18e08f994369
SIOUkoeran <alstnalsgud@gmail.com>
2021-11-21 12:29:28 +0900

    PEER-20 Add 피드백 테스트 설정 파일

---
+++ src/test/java/com/seoul/feedback/common/BaseControllerTest.java
@@ -0,0 +1,14 @@
+package com.seoul.feedback.common;
+
+
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(com.seoul.feedback.coomon.RestDocsConfiguration.class)
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@SpringBootTest
+public class BaseControllerTest {
+}
