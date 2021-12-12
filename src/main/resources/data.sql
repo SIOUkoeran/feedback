8e16755fda913467cbdbddec02286c99101873b7
SIOUkoeran <alstnalsgud@gmail.com>
2021-12-12 15:31:11 +0900

    Add 초기 데이터 적재 PEER-8

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user(_id, login, created_at, role, updated_at) values(1, 'mkim3', '2021-12-12 00:02:03.123123', 'STUDENT', '2021-12-12 00:02:03.123123');
-insert into user(_id, login, created_at, role, updated_at) values(2, 'seokim', '2021-12-12 00:02:03.123123', 'STUDENT', '2021-12-12 00:02:03.123123');
-insert into user(_id, login, created_at, role, updated_at) values(3, 'siokim', '2021-12-12 00:02:03.123123', 'STUDENT', '2021-12-12 00:02:03.123123');
-insert into user(_id, login, created_at, role, updated_at) values(4, 'womkim', '2021-12-12 00:02:03.123123', 'STUDENT', '2021-12-12 00:02:03.123123');
-insert into user(_id, login, created_at, role, updated_at) values(5, 'mkim2', '2021-12-12 00:02:03.123123', 'STUDENT', '2021-12-12 00:02:03.123123');
