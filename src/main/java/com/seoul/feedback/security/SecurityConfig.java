a5892cc0a5adb06d7581326ddee32ed4e0da5a12
SIOUkoeran <alstnalsgud@gmail.com>
2021-12-16 16:09:26 +0900

    Fix application.yml DB접속 옵션 변경 PEER-27

--- src/main/java/com/seoul/feedback/security/SecurityConfig.java
+++ src/main/java/com/seoul/feedback/security/SecurityConfig.java
@@ -1,17 +1,9 @@
 package com.seoul.feedback.security;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.file.ConfigurationSource;
-import org.hibernate.cfg.Configuration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Collections;
 
 
 @RequiredArgsConstructor
@@ -32,8 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/css/**", "/images/**", "/js/**", "/h2/**", "/h2-console/**", "/docs/**", "/oauth2/**").permitAll()
-                .anyRequest().hasAnyRole("STUDENT")
+                .antMatchers("/api/**").hasRole("STUDENT")
+                .anyRequest().authenticated()
                 .and()
                 .logout()
                     .logoutSuccessUrl("/").permitAll()
@@ -46,17 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(oAuth2SuccessHandler)
                 ;
     }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Collections.singletonList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+
 
 
 }
