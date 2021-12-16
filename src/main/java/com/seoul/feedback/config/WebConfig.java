5b917c75b3f90bac5c99b2274ff30f9d9e089fbc
SIOUkoeran <alstnalsgud@gmail.com>
2021-12-16 16:30:37 +0900

    Fix CorsFilter 추가 PEER-27

--- src/main/java/com/seoul/feedback/config/WebConfig.java
+++ src/main/java/com/seoul/feedback/config/WebConfig.java
@@ -1,20 +1,20 @@
-package com.seoul.feedback.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    private final long MAX_AGE_SECS = 3600;
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .maxAge(MAX_AGE_SECS);
-    }
-}
+//package com.seoul.feedback.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    private final long MAX_AGE_SECS = 3600;
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .maxAge(MAX_AGE_SECS);
+//    }
+//}
