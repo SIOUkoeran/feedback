79b34215cd0e83862b98523760e169c0b727bb91
SIOUkoeran <alstnalsgud@gmail.com>
2021-12-16 17:29:37 +0900

    Fix webconfig 설정 변경 PEER-27

--- src/main/java/com/seoul/feedback/config/WebConfig.java
+++ src/main/java/com/seoul/feedback/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .maxAge(MAX_AGE_SECS);
     }
