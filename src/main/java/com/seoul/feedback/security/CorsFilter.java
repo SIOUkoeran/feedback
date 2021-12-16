f0d70a2e7374e86bd93faa90d03571c6735a311e
SIOUkoeran <alstnalsgud@gmail.com>
2021-12-16 16:47:40 +0900

    Fix header 제거 PEER-27

--- src/main/java/com/seoul/feedback/security/CorsFilter.java
+++ src/main/java/com/seoul/feedback/security/CorsFilter.java
@@ -26,7 +26,6 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "True");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
